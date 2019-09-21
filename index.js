493a61ef6147535b68b7377028f9abb17984f692
Sarah Loach <Sarah@Sarahs-fat-top.local>
2019-09-21 18:31:25 -0500

    completed updates for challenge assessment

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-$(document).ready(() => {
-    
-})
+$(function (){
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        let newItem = $('#shopping-list-entry').val();
+        let listItem = 
+        '<li>' +
+            '<span class="shopping-item">' + newItem + '</span>' +
+            '<div class="shopping-item-controls">' +
+            '<button class="shopping-item-toggle">' +
+                '<span class="button-label">check</span>' +
+            '</button>' +
+            '<button class="shopping-item-delete">' +
+                '<span class="button-label">delete</span>' +
+            '</button>' +
+            '</div>'+
+        '</li>'
+        $('.shopping-list').append(listItem);
+    });
+
+    $('.shopping-item-toggle').click(function(event){
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+    $('.shopping-item-delete').click(function(event){
+        $(this).closest('li').remove();
+    });
+        
+});
+
